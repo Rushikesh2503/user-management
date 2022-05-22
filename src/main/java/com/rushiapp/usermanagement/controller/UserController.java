87b9dc7027f67afb5ce59e309df36a044725588f
Rushikesh2503 <rushikeshbambadkar7@gmail.com>
2022-05-22 19:07:59 +0530

    first commit

---
+++ src/main/java/com/rushiapp/usermanagement/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.rushiapp.usermanagement.controller;
+
+import com.rushiapp.usermanagement.model.User;
+import com.rushiapp.usermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public User saveUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id){
+        User user=null;
+        user=userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteUser(@PathVariable("id") Long id){
+        boolean deleted=false;
+        deleted=userService.deleteUser(id);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted User",deleted);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user ){
+
+        user=userService.updateUser(id,user);
+
+        return ResponseEntity.ok(user);
+
+    }
+
+}
---
+++ src/main/java/com/rushiapp/usermanagement/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rushiapp.usermanagement.repository;
+
+import com.rushiapp.usermanagement.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+
+}
